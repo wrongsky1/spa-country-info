6bfaa1c8956a40d76f97ab621539ab3438756130
Misha <wrongsky1@yandex.ru>
2022-09-16 15:47:50 +0300

    add reducer for details component

---
+++ src/store/details/details-reducer.js
@@ -0,0 +1,32 @@
+import { SET_COUNTRY, SET_ERROR, SET_LOADING } from "./details-actions";
+
+const initialState = {
+    currentCountry: null,
+    status: "idle",
+    error: null,
+};
+
+export const detailReducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+        case SET_LOADING:
+            return {
+                ...state,
+                error: null,
+                status: "loading",
+            };
+        case SET_ERROR:
+            return {
+                ...state,
+                error: payload,
+                status: "rejected",
+            };
+        case SET_COUNTRY:
+            return {
+                ...state,
+                status: "received",
+                currentCountry: payload,
+            };
+        default:
+            return state;
+    }
+};
