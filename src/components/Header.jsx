75fe825d30433a43d01dfd58d5a05666b5d0c334
Misha <wrongsky1@yandex.ru>
2022-09-16 15:01:07 +0300

    add clear controls action

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import { IoMoon, IoMoonOutline } from 'react-icons/io5';
 
 import { Container } from './Container';
 import { setTheme } from '../store/theme/theme-actions';
+import { clearControls } from '../store/controls/controls-actions';
 
 const HeaderEl = styled.header`
   box-shadow: var(--shadow);
@@ -41,7 +42,8 @@ export const Header = () => {
   const dispatch = useDispatch();
   const theme = useSelector(state => state.theme);
 
-  const toggleTheme = () => dispatch(setTheme(theme === 'light' ? 'dark' : 'light'))
+  const toggleTheme = () => dispatch(setTheme(theme === 'light' ? 'dark' : 'light'));
+  const cleanUp = () => dispatch(clearControls())
 
   useEffect(() => {
     document.body.setAttribute('data-theme', theme);
@@ -51,7 +53,7 @@ export const Header = () => {
     <HeaderEl>
       <Container>
         <Wrapper>
-          <Title>Where is the world?</Title>
+          <Title onClick={cleanUp}>Where is the world?</Title>
           <ModeSwitcher onClick={toggleTheme}>
             {theme === 'light' ? (
               <IoMoonOutline size="14px" />
