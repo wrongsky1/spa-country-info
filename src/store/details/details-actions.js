5e377dd54bb46f394b4445ad760cf037421a1fca
Misha <wrongsky1@yandex.ru>
2022-09-16 16:17:15 +0300

    add border countries to details page

--- src/store/details/details-actions.js
+++ src/store/details/details-actions.js
@@ -2,6 +2,7 @@ export const SET_LOADING = "@@details/SET_LOADING";
 export const SET_ERROR = "@@details/SET_ERROR";
 export const SET_COUNTRY = "@@details/SET_COUNTRY";
 export const CLEAR_DETAILS = "@@details/CLEAR_DETAILS";
+export const SET_NEIGHBORS = "@@details/SET_NEIGHBORS";
 
 const setLoading = () => ({
     type: SET_LOADING,
@@ -14,6 +15,10 @@ const setCountry = (country) => ({
     type: SET_COUNTRY,
     payload: country,
 });
+const setNeighbors = (countries) => ({
+    type: SET_NEIGHBORS,
+    payload: countries,
+});
 export const clearDetails = () => ({
     type: CLEAR_DETAILS,
 });
@@ -25,4 +30,11 @@ export const loadCountryByName =
             .get(api.searchByCountry(name))
             .then(({ data }) => dispatch(setCountry(data[0])))
             .catch((err) => dispatch(setError(err.message)));
+    };
+export const loadNeighborsByCode =
+    (borders) =>
+    (dispatch, _, { client, api }) => {
+        client.get(api.filterByCode(borders))
+        .then(({ data }) => dispatch(setNeighbors(data.map(c => c.name))))
+        .catch(console.error);
     };
